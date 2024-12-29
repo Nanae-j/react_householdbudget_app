01c44ccadd55046eba90b0eb22a54037b61939b1
Nanae-j <nanae9577@gmail.com>
2024-12-29 15:42:46 +0900

    [add]firestore DB のセットアップ

---
+++ src/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyA6yeixJ5X1FOwWfSgDMhqfeI1ABVtNZrA',
+  authDomain: 'household-typescript-8acbe.firebaseapp.com',
+  projectId: 'household-typescript-8acbe',
+  storageBucket: 'household-typescript-8acbe.firebasestorage.app',
+  messagingSenderId: '77468511132',
+  appId: '1:77468511132:web:ef0a30b56aaab47db13659',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
