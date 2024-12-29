07434d924fa850acc3f70d35ab6023b2bbe6ff34
Nanae-j <nanae9577@gmail.com>
2024-12-29 20:58:42 +0900

    [add]Fullcalenderのインポート

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,7 +1,49 @@
 import React from 'react';
+import FullCalender from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import jaLocale from '@fullcalendar/core/locales/ja';
+import { EventContentArg } from '@fullcalendar/core';
+import '../Calendar.css';
 
 const Calendar = () => {
-  return <div>Calendar</div>;
+  const events = [
+    {
+      title: 'Meeting',
+      start: new Date(),
+      income: 300,
+      expense: 500,
+      balance: 200,
+    },
+    { title: 'Meeting', start: '2024-12-29' },
+    { title: 'Meeting', start: '2024-12-30' },
+  ];
+
+  const renderEventContent = (eventInfo: EventContentArg) => {
+    console.log(eventInfo);
+    return (
+      <div>
+        <div className="money" id="event-income">
+          {eventInfo.event.extendedProps.income}
+        </div>
+        <div className="money" id="event-expense">
+          {eventInfo.event.extendedProps.expense}
+        </div>
+        <div className="money" id="event-balance">
+          {eventInfo.event.extendedProps.balance}
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <FullCalender
+      locale={jaLocale}
+      plugins={[dayGridPlugin]}
+      initialView="dayGridMonth"
+      events={events}
+      eventContent={renderEventContent}
+    />
+  );
 };
 
 export default Calendar;
